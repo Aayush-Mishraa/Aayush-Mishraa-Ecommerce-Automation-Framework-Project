892000663ec13e06d97b23e0bd3b9869ec36fbdc
Aayush_MishraaAayush-Mishraa <aayushmishra026@gmail.com>
2024-04-07 19:25:19 +0530

    First commit

---
+++ src/test/java/Ecom/Ecommerce_Automation_Framework_Project/AppTest.java
@@ -0,0 +1,20 @@
+package Ecom.Ecommerce_Automation_Framework_Project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
